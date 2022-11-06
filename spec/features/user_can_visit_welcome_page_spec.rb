a9ec0fe5ce4d3c5bd99742cdc48a76a2560af7d5
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-06 09:20:39 -0500

    initial commit

--- spec/features/user_can_visit_welcome_page_spec.rb
+++ spec/features/user_can_visit_welcome_page_spec.rb
@@ -7,4 +7,17 @@ describe "user can visit the welcome page" do
     expect(page).to have_content("Search For The Nearest Electric Charging Station")
     expect(page).to have_button("Find Nearest Station")
   end
+
+  describe 'when I visit the welcome page' do
+    before { visit '/' }
+
+    describe 'when I select Turing as a start location' do
+      it 'takes me to /search' do
+        select "Turing", from: :location
+        click_on "Find Nearest Station"
+
+        expect(current_path).to eq('/search')
+      end
+    end
+  end
 end
